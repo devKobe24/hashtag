7b100d12b5e8dcef196efd034cde68db59b2c209
Kobe <dev.skyachieve91@gmail.com>
2023-11-04 15:37:21 +0900

    :sparkles: Feat: Color Extension 생성 및 구현.
    Color 을 확장하여 Assets에 생성한 커스텀 컬러를 호출할 수 있도록 구현하였습니다.

---
+++ Hashtag/Hashtag/Extensions/Color+.swift
@@ -0,0 +1,14 @@
+//
+//  Color+.swift
+//  Hashtag
+//
+//  Created by Minseong Kang on 11/4/23.
+//
+
+import SwiftUI
+
+extension Color {
+    #if os(iOS)
+    static let brandPrimaryColor = Color("brandPrimary")
+    #endif
+}
