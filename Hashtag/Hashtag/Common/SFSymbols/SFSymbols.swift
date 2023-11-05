10f35974c85df88616cdbc9bafa8cd0d445ca1db
Kobe <dev.skyachieve91@gmail.com>
2023-11-05 15:37:05 +0900

    :recycle: Refactor: SFSymbols 케이스 5가지 추가.
    mappinAndEllipse, locationFill, network, listClipboardFill, personFillCheckmark 케이스를 추가하였습니다.

--- Hashtag/Hashtag/Common/SFSymbols/SFSymbols.swift
+++ Hashtag/Hashtag/Common/SFSymbols/SFSymbols.swift
@@ -9,6 +9,11 @@ enum SFSymbols {
     case map
     case building
     case person
+    case mappinAndEllipse
+    case locationFill
+    case network
+    case listClipboardFill
+    case personFillCheckmark
     
     var imageName: String {
         switch self {
@@ -18,6 +23,16 @@ enum SFSymbols {
             return "building"
         case .person:
             return "person"
+        case .mappinAndEllipse:
+            return "mappin.and.ellipse"
+        case .locationFill:
+            return "location.fill"
+        case .network:
+            return "network"
+        case .listClipboardFill:
+            return "list.clipboard.fill"
+        case .personFillCheckmark:
+            return "person.fill.checkmark"
         }
     }
 }
